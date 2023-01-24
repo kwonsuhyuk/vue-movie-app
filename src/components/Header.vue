b7e62f3fbc192de09f0a1990cc11ff2cec9d4f89
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2023-01-24 22:35:55 +0900

    movie netlify function

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -61,7 +61,6 @@ export default {
   methods : {
     isMatch(path) {
       if (!path) return false
-      console.log(this.$route)
       return path.test(this.$route.fullPath)
     },
     toAbout() {
--- src/store/movie.js
+++ src/store/movie.js
@@ -70,7 +70,7 @@ export default {
             })
           }
         }
-      }catch(message){
+      }catch({message}){
         commit('updateState', {
           message,
           movies : [],
@@ -109,25 +109,6 @@ export default {
 }
 
 
-function _fetchMovie(payload) {
-  const {title, type, page, year, id} = payload;
-  const OMDB_API_KEY = '7035c60c';
-  const url = id
-    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` 
-    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-  
-  return new Promise((resolve, reject)=> {
-    axios.get(url)
-      .then((response) => {
-        if (response.data.Error) {
-          reject(response.data.Error)
-        }
-        resolve(response)
-      })
-      .catch((err) => {
-        reject(err.message)
-      })
-      
-  })
-
+async function _fetchMovie(payload) {
+  return await axios.post('/.netlify/functions/movie', payload) 
 }
